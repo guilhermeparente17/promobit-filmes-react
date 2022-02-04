0ae109b23001712edbe88746ed2dbb7055ce97e7
Guilherme Parente <guilherme.parente17@outlook.com>
2022-02-04 10:39:08 -0300

    teste

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Categorias from '../../components/Categorias/Categorias';
 import Filmes from '../../components/Filmes/Filmes';
 import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
 
 const Home = () => {
     return (
